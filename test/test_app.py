30aec1a3e5f102dbbd4a323a8da7e76483641184
Joao Pedro <joaopioonze@gmail.com>
2026-04-30 11:49:23 -0300

    test: adiciona mais testes em todas as rotas

--- test/test_app.py
+++ test/test_app.py
@@ -8,13 +8,13 @@ from app import app
 client = TestClient(app)
 
 
-def test_read_root():
+def test_read_root_sucess():
     response = client.get("/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello Word"}
 
 
-def test_create_user(client_override, session):
+def test_create_user_success(client_override, session):
     response = client_override.post(
         "/create/", json={"username": "test", "age": 15, "email": "test@example.com"}
     )
@@ -37,22 +37,83 @@ def test_create_user(client_override, session):
     assert new_user.email == "test@example.com"
 
 
+def test_create_user_name_error(client_override, add_user_database, session):
+    response = client_override.post(
+        "/create/", json={"username": "GhostUser", "age": 15, "email": "test@example.com"}
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+
+def test_create_user_email_error(client_override, add_user_database, session):
+    response = client_override.post(
+        "/create/", json={"username": "teste", "age": 15, "email": "ghost_user@example.com"}
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+
+
 def test_read_users(session=get_db):
     response = client.get("/read/")
     assert response.status_code == HTTPStatus.OK
-    assert response.json() is not None
 
 
 def test_update_users(client_override, add_user_database, session):
+    existing_user = session.scalar(
+        select(Users).where(
+           (Users.id == 1)
+        )
+    )
+
     response = client_override.put(
         "/update/1", json={"username": "teste", "age": 0, "email": "teste@example.com"}
     )
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json()["username"] != session.scalar
-    
 
-def test_search_users(client_override, add_user_database, session):
+
+def test_update_users_id_error(client_override, add_user_database, session):
+    existing_user = session.scalar(
+        select(Users).where(
+           (Users.id == 1)
+        )
+    )
+
+    response = client_override.put(
+        "/update/0", json={"username": "teste", "age": 0, "email": "teste@example.com"}
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_update_users_email_error(client_override, add_user_database, session):
+    existing_user = session.scalar(
+        select(Users).where(
+           (Users.id == 1)
+        )
+    )
+
+    response = client_override.put(
+        "/update/1", json={"username": "teste", "age": 0, "email": "ghost_user@example.com"}
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+
+
+def test_update_users_name_error(client_override, add_user_database, session):
+    existing_user = session.scalar(
+        select(Users).where(
+           (Users.id == 1)
+        )
+    )
+
+    response = client_override.put(
+        "/update/1", json={"username": "GhostUser", "age": 0, "email": "teste@example.com"}
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+
+
+def test_search_users_success(client_override, add_user_database, session):
     existing_user = session.scalar(
         select(Users).where(
            (Users.id == 1)
@@ -64,10 +125,18 @@ def test_search_users(client_override, add_user_database, session):
     )
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json()["id"] == existing_user.id
+    assert response.json()["username"] == existing_user.username
 
 
-def test_delete_user(client_override, add_user_database, session):
+def test_search_users_id_error(client_override, add_user_database, session):
+    response = client_override.get(
+       "/search/0"
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user_success(client_override, add_user_database, session):
     existing_user = session.scalar(
         select(Users).where(
            (Users.id == 1)
@@ -79,4 +148,12 @@ def test_delete_user(client_override, add_user_database, session):
     )
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json()["id"] == existing_user.id
+    assert response.json()["username"] == existing_user.username
+
+
+def test_delete_user_id_error(client_override, add_user_database, session):
+    response = client_override.delete(
+        "/delete/0"
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
